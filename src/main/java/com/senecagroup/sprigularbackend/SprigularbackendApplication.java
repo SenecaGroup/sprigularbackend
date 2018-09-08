a917e4a68c7a8c975abcaff6d9074dc56f5a0957
HOMEPC <skim327@myseneca.ca>
2018-09-08 11:14:22 -0400

    First Structure Project

---
+++ src/main/java/com/senecagroup/sprigularbackend/SprigularbackendApplication.java
@@ -0,0 +1,12 @@
+package com.senecagroup.sprigularbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigularbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprigularbackendApplication.class, args);
+    }
+}
