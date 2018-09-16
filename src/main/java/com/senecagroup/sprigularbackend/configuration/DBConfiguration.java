edda38168e29d1a224949c9bd67973b25cc84547
HOMEPC <skim327@myseneca.ca>
2018-09-16 19:08:07 -0400

    20180916:19:07 siwookim - CONFIGURING DATABASE

--- src/main/java/com/senecagroup/sprigularbackend/configuration/DBConfiguration.java
+++ src/main/java/com/senecagroup/sprigularbackend/configuration/DBConfiguration.java
@@ -1,20 +1,106 @@
 package com.senecagroup.sprigularbackend.configuration;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class DBConfiguration {
 
+    @Autowired
+    private Environment env;
+
+    @Value("${db.driver}")
+    private String DRIVER;
+
+    @Value("${db.url}")
+    private String URL;
+
+    @Value("${db.username}")
+    private String USERNAME;
+
+    @Value("${db.password}")
+    private String PASSWORD;
+
+    private final Logger log = LoggerFactory.getLogger(DBConfiguration.class);
+
+
     @Bean
-    DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .build();
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        log.debug("Configuring Datasource");
+        BasicDataSource dataSourceConfig = new BasicDataSource();
+        dataSourceConfig.setDriverClassName(DRIVER);
+        dataSourceConfig.setUrl(URL);
+        dataSourceConfig.setUsername(USERNAME);
+        dataSourceConfig.setPassword(PASSWORD);
+
+        return dataSourceConfig;
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(Boolean.TRUE);
+        vendorAdapter.setShowSql(Boolean.FALSE);
+        factory.setDataSource(dataSource());
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.senecagroup.sprigularbackend");
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.show_sql", "true");
+        jpaProperties.put("hibernate.enable_lazy_load_no_trans", "true");
+        jpaProperties.put("hibernate.max_fetch_depth", "0");
+        jpaProperties.put("hibernate.current_session_context_class", "thread");
+        jpaProperties.put("hibernate.event.merge.entity_copy_observer", "allow");
+        jpaProperties.put("hibernate.hbm2ddl.auto", "create");
+
+        factory.setJpaProperties(jpaProperties);
+        factory.afterPropertiesSet();
+        factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
+    @Bean
+    public HibernateExceptionTranslator hibernateExceptionTranslator() {
+        return new HibernateExceptionTranslator();
     }
 
 }
